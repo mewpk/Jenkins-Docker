487b50f55af8e0aa9fa5ca23cfd27b219a656464
mewpk <psk.mewpk@gmail.com>
2025-08-24 23:21:56 +0700

    Update Dockerfile to use debian:bookworm and modify run.sh permissions

--- run.sh
+++ run.sh
@@ -1,5 +1,19 @@
+#!/bin/bash
+
+echo "Building Jenkins master image..."
 docker build -t jenkins-master -f master/Dockerfile .
+
+echo "Building Jenkins data image..."
 docker build -t jenkins-data -f data/Dockerfile .
+
+echo "Creating Jenkins data container..."
 docker run --name=jenkins-data jenkins-data
+
+echo "Starting Jenkins master container..."
 docker run -p 8080:8080 -p 50000:50000 --name=jenkins-master --volumes-from=jenkins-data -d jenkins-master
+
+echo "Listing all containers..."
 docker ps -a
+
+echo "Fetching Jenkins initial admin password..."
+docker exec jenkins-master cat /var/jenkins_home/secrets/initialAdminPassword
